0fc1cb9350a2973f85c3c9fca37b8c178f42ee63
Laia <laiaperpra@campus.monlau.com>
2026-05-21 11:57:03 +0200

    Update api.php

--- api.php
+++ api.php
@@ -1,12 +1,1 @@
-<?php
-header("Content-Type: application/json");
-header("Access-Control-Allow-Origin: *");
 
-echo json_encode([
-    "message" => "API del CV funcionando correctamente",
-    "endpoints" => [
-        "GET /backend/index.php" => "Datos completos del CV",
-        "GET /backend/api.php" => "Este mensaje de prueba"
-    ]
-]);
-?>
